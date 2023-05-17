84ff7d2667ddb5e19799b6a2f1544f27370f31e4
jparsio <jpthecoder@gmail.com>
2023-05-17 09:48:23 -0500

    .

--- a7.py
+++ a7.py
@@ -1,6 +1,7 @@
 import csv
 from neural import NeuralNet 
 import neural_net_UCI_data as nn_uci
+from sklearn.model_selection import train_test_split
 
 voter_opinion = [
     ([0.9,0.6,0.8,0.3,0.1], [1.0]),
@@ -11,11 +12,23 @@ voter_opinion = [
     ([0.6,0.3,0.4,0.3,0.6], [0.0])
 ]
 
-von = NeuralNet(5,10,1)
 
 
-file = open("rice.txt", "r").readlines()
+file = open("rice.csv", "r").readlines()
 vals = [nn_uci.parse_line(line) for line in file]
 vals = nn_uci.normalize(vals)
-print(vals)
+train, test = train_test_split(vals, test_size=0.01)
+rice_net = NeuralNet(7, 10, 1)
+rice_net.train(train)
+results = rice_net.test_with_expected(test)
 
+total = 0
+results = [(result[1][0], result[2][0]) for result in results]
+results.sort()
+for result in results:
+    expected = result[0]
+    actual = result[1]
+    difference = abs(expected - actual)
+    total += difference
+    print(expected, actual)
+print("average difference: ", total/len(results))
